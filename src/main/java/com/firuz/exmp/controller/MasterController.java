9ed7f8a5195551680c6a39dc084f7136aaf69f0e
Firuz43 <firuz1199@mail.ru>
2022-05-04 11:15:56 +0400

    jsp file

--- src/main/java/com/firuz/exmp/controller/MasterController.java
+++ src/main/java/com/firuz/exmp/controller/MasterController.java
@@ -1,10 +1,13 @@
 package com.firuz.exmp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MasterController {
+	
+	@RequestMapping("/")
 	public ModelAndView home() {
 		ModelAndView mav = new ModelAndView("home");
 		return mav;
